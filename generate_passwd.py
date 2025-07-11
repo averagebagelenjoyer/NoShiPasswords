4964e80a5e262850de18d89e80b930e70fb91e34
averagebagelenjoyer <211752292+averagebagelenjoyer@users.noreply.github.com>
2025-07-10 23:00:48 -0700

    added buffer support

--- generate_passwd.py
+++ generate_passwd.py
@@ -6,7 +6,8 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 import re # required for error detection
 import rstr # very very important for the password generation
 import pyperclip # clipboard stuff
-import configparser # important for the memes
+import configparser # important for the memes ALSO required for buffer
+import time
 
 # uh- ok this feature is genuinely just a joke
 config = configparser.ConfigParser()
@@ -23,6 +24,9 @@ try:
         try:
             if regex.startswith("!"):
                 pyperclip.copy(rstr.xeger(regex[1:]))
+                if config.getboolean("general", "clipboard_buffer", fallback=False):
+                    time.sleep(1)
+                    pyperclip.copy("Buffer")
                 print("Copied to clipboard")
             else:
                 print(rstr.xeger(regex))
