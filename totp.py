28a70313bc27460a24e195500ff2030732a48a38
averagebagelenjoyer <211752292+averagebagelenjoyer@users.noreply.github.com>
2025-07-17 11:38:07 -0700

    removed flags

--- totp.py
+++ totp.py
@@ -2,7 +2,6 @@ import os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 from pwinput import pwinput # it's like `getpass` but better
-import argparse # required for command line and GUI
 
 # all of this is required for the TOTP
 import base64
@@ -10,14 +9,7 @@ import hashlib
 import hmac
 import struct
 import time
-
 import sys
-
-parser = argparse.ArgumentParser(description="Example: greet user with optional shout")
-parser.add_argument("-c", "--commandLine", action="store_true", help="This must be set if used in the command line")
-parser.add_argument("-S", "--secret", help="TOTP secret")
-
-args = parser.parse_args()
 
 def get_totp_token(secret, interval=30, digits=6):
     try:
@@ -30,16 +22,6 @@ def get_totp_token(secret, interval=30, digits=6):
         return str(token).zfill(digits)
     except:
         return False
-
-if args.commandLine:
-    token = get_totp_token(args.secret)
-
-    if token:
-        print(token)
-        sys.exit(0)
-    else:
-        print("invalid secret", sys.stderr)
-        sys.exit(2)
 
 while True:
     secret = pwinput("TOTP secret (Leave empty to exit): ")
